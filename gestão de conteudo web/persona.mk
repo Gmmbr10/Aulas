48b1b28fbb0f20d7bb1ea3e271862d97bbf239a9
Giovanne Monteiro <101524886+Gmmbr10@users.noreply.github.com>
2022-08-15 18:35:36 -0300

    Add files via upload

---
+++ gestão de conteudo web/persona.mk
@@ -0,0 +1,38 @@
+# Persona
+
+## O que é Persona?
+
+Personagem semifictício gerado à partir de dados de pessoas reais.
+
+Esses dados são coletados após uma pesquisa detalhada.
+Vindas de perguntas como
+
+─ Com o que trabalha?
+
+─ Quem é essa pessoa?
+
+─ O que ela faz?
+
+─ Do que ela precisa?
+
+## Para qual finalidade eu utilizo a Persona?
+
+Para gerar conteúdos específicos e fazer com que o nosso público seja bem definido.
+
+## Qual a diferença do Público Alvo para a Persona?
+
+O público alvo é bem superficial.
+
+No público alvo se procura
+
+- Estado civil, - Formação, - Hábito(s) de compra, etc;
+
+Na Persona
+
+- Como é o seu dia?, - Nome, - Redes sociais favorita, - Ambições, etc;
+
+A persona busca entender como é a cabeça do público trabalhado.
+
+## Como eu faço uma persona?
+
+![Como fazer uma persona](https://images.app.goo.gl/5wvG7G1hhJov9gow8)
